b552ce215b87749527755371df6c0d884ae23b3c
LawrBoni <31030984+Hikozak@users.noreply.github.com>
2017-10-03 16:55:16 +0800

    Initial commit

---
+++ src/app/removeperson/removeperson.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-removeperson',
+  templateUrl: './removeperson.component.html',
+  styleUrls: ['./removeperson.component.css']
+})
+export class RemovepersonComponent implements OnInit {
+  baseURL = 'https://firstprojectdb.firebaseio.com/';
+  rootNode = 'people';
+  refID: any;
+
+  fname: string;
+  lname: string;
+
+  person: object;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
